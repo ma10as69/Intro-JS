7ca9a467da933437f9f5508b93998fd12b4a223f
ma10as69 <mathan35@vgs.nfk.no>
2022-09-05 10:00:29 +0200

    made random number var and if

--- Guessing game/numberguess.js
+++ Guessing game/numberguess.js
@@ -1,11 +1,28 @@
 
+var number = Math.floor(Math.random() * 100) + 1;
+
+
     var knapp = document.querySelector("#knappID");
     var input = document.querySelector("#inputID");
 
     function sjekkTall(){
     console.log(input.value)
+
+    if (knapp === number) {
+        console.log("Riktig!");
     }
+    if (knapp !== number) {
+        console.log("Feil!");
+    }
+       }
+    if (knapp === number) {
+        console.log("Riktig!");
+    } else {
+    console.log("Feil!");
+    }
+    
+    
 
 
     knapp.addEventListener("click", sjekkTall);
-    
+                                              
